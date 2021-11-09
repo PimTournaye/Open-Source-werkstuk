1cac2b759e704754dca038c79556684c1eee4f1e
PimTournaye <pim.tournaye@student.ehb.be>
2021-11-10 00:02:32 +0100

    set up express template

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-// TODO:
-// set up express server here
+import express from "express";
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
