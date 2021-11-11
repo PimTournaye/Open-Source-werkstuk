76320ac254680d22d77bbf8877b99c3678299820
PimTournaye <pim.tournaye@student.ehb.be>
2021-11-11 22:04:25 +0100

    laid out plan for end2end test

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -5,6 +5,8 @@ import express from "express";
 const app = express()
 const PORT = 3000
 
+const DATABASE = 'testing';
+
 // Importing for the algorithm
 import mode from '../src/logic/Mode';
 import note from '../src/logic/Note';
@@ -25,3 +27,16 @@ const pg = require('knex')({
   searchPath: ['knex', 'public'],
   connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://user:pass@localhost:5432/db'
 });
+
+// Create new table
+// try to delete a row
+// insert a new row
+
+describe('Postgres testing', () => {
+  
+  it('should create a new table', async () => {
+    await pg.raw(`DROP DATABASE IF EXISTS ${DATABASE}`)
+    await pg.raw(`CREATE DATABASE ${DATABASE}`)
+
+  })
+})
