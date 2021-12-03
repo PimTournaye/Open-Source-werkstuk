6e417c998865a587c9f65f22ac7b1e254f6c5970
PimTournaye <pim.tournaye@student.ehb.be>
2021-12-03 11:14:15 +0100

    docs: Added JSDoc to some routes

--- src/index.ts
+++ src/index.ts
@@ -56,31 +56,62 @@ app.put('/update', (req, res) => {
 })
 
 // GET endpoints, giving back various combinations of sounds.
+
+/**
+ * GET Route - Note
+ * Generates a note in the current key and mode.
+ * @returns a single note in the form of a string, ie. "C3"
+ */
 app.get('/note', (req, res) => {
   const note = small.onPress()
   res.send(note)
 })
 
+/**
+ * GET Route - Chord
+ * Generates a 3-note or 4-note chord in  the current key, while also changing the music's mode.
+ * @returns an array of 3 or 4 strings
+ */
 app.get('/chord', (req, res) => {
   const notes = chord.onPress()
   res.send(notes)
 })
 
+/**
+ * GET Route - Vamp
+ * Generates a 3-note or 4-note chord in the current key and mode.
+ * @returns an array of 3 or 4 strings
+ */
 app.get('/vamp', (req, res) => {
   const notes = vamp.onPress()
   res.send(notes)
 })
 
+/**
+ * GET Route - Octave
+ * Generates a note and an additional one an octave higher or lower
+ * @returns an array of two strings
+ */
 app.get('/octave', (req, res) => {
   const notes = octave.onPress()
   res.send(notes)
 })
 
+/**
+ * GET Route - Octave
+ * Generates a note and an additional one that is the first one's harmonic.
+ * @returns an array of two strings
+ */
 app.get('/harmony', (req, res) => {
   const notes = harmony.onPress()
   res.send(notes)
 })
 
+/**
+ * GET Route - Transpose
+ * Changes the whole feel of the generated music. Changes the current key and mode and then generates a chord, an octave tone and an harmony tone. Big sound boom. Yay!
+ * @returns an array of 5-6 strings.
+ */
 app.get('/transpose', (req, res) => {
   const notes = transpose.onPress()
   res.status(200).json(notes)
