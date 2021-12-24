bcea59f15496dfd69e9c3c7ef46ca7f782bf6671
PimTournaye <pim.tournaye@student.ehb.be>
2021-12-24 03:26:46 +0100

    test: unit tests done

--- app/__tests__/music.test.ts
+++ app/__tests__/music.test.ts
@@ -7,6 +7,10 @@ import mode from '../src/logic/Mode';
 import note from '../src/logic/Note';
 import small from '../src/actions/small.action';
 import intervals from '../src/logic/Intervals';
+import octave from '../src/actions/octave.action';
+import harmony from '../src/actions/harmony.action';
+import key from '../src/logic/Key';
+import transpose from '../src/actions/transpose.action';
 
 
 // Music algorithm init
@@ -44,4 +48,40 @@ test('check for different intervals loadout', () => {
 
     expect(firstLoadout).not.toBe(newLoadout);
 
+})
+
+test('check for different octave', () => {
+    small.onPress();
+    const firstOctave = octave.playOctave();
+    const secondOctave = octave.playOctave();
+    expect(firstOctave).not.toBe(secondOctave);
+});
+
+test('check for different harmony', () => {
+    small.onPress();
+    const firstHarmony = harmony.playHarmonyTone();
+    const secondHarmony = harmony.playHarmonyTone();
+    expect(firstHarmony).not.toBe(secondHarmony);
+})
+test('check for different everything', () => {
+    small.onPress();
+    const firstHarmony = harmony.playHarmonyTone();
+    const firstOctave = octave.playOctave();
+    const currentMode = mode.current;
+    const firstLoadout = intervals.loadout;
+    const currentKey = key.current[0];
+
+    transpose.onPress()
+
+    const secondtHarmony = harmony.playHarmonyTone();
+    const secondOctave = octave.playOctave();
+    const newMode = mode.current;
+    const newLoadout = intervals.loadout;
+    const newKey = key.current[0];
+
+    expect(firstHarmony).not.toBe(secondtHarmony)
+    expect(firstOctave).not.toBe(secondOctave)
+    expect(currentMode).not.toBe(newMode)
+    expect(firstLoadout).not.toBe(newLoadout)
+    expect(currentKey).not.toBe(newKey)
 })
